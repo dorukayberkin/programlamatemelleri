1b1e26a77e4811f771adc36eca5c508fd368b5c4
Doruk Ayberkin <bluedor@hotmail.com>
2018-10-18 17:23:05 +0300

    Add files via upload

---
+++ Recovered-Eki-05-2018-0642ÖS.SadeceSayiGir.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temel_Kodlama_Örnekleri
+{
+    public class SadeceSayiGir
+    {
+        public static void SayiKontrol(string sayi)
+        {
+            double val = 0;
+            string num = "";
+            Console.Write(sayi);
+            ConsoleKeyInfo chr;
+           
+                chr = Console.ReadKey(true);
+                if (chr.Key != ConsoleKey.Backspace)
+                {
+                    bool control = double.TryParse(chr.KeyChar.ToString(), out val);
+                    if (control)
+                    {
+                        num += chr.KeyChar;
+                        Console.Write(chr.KeyChar);
+                    }
+                }
+                else
+
+                {
+                    if (chr.Key == ConsoleKey.Backspace && num.Length > 0)
+                    {
+                        num = num.Substring(0, (num.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                }
+           
+        }
+    }  
+}
---
+++ Recovered-Eki-05-2018-0642ÖS.SayiSistemDonusumleri.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Temel_Kodlama_Örnekleri
+{
+    public class SayiSistemDonusumleri
+    {
+        public static void IkilikSayiyiOnlugaCevir()
+        {
+        tekrar:
+            Console.WriteLine("İkilik sayı sisteminde bir sayı girin");
+            string sayi = Console.ReadLine();
+            char[] ikilik = sayi.ToCharArray();           
+            Array.Reverse(ikilik);
+            foreach (char basamak in ikilik)
+            {
+                if ((basamak != '0') && (basamak != '1'))
+                {
+                    Console.WriteLine("Yanlış giriş yaptınız");
+                    goto tekrar;
+                }
+            }
+            double onluk = 0;
+            int sira = 0;
+
+            foreach (var basamak in ikilik)
+            {
+                onluk += Math.Pow(2, sira) * Convert.ToInt32(basamak.ToString());
+                sira++;
+            }
+            
+            Console.WriteLine("Girilen İkilik Sistemdeki Sayının Onluk Sistemdeki Kaşılığı ={0}", onluk);
+            Console.ReadKey();
+        }
+
+        public static void OnlukSayiyiIkiligeCevir()
+        {
+            Console.WriteLine("Onluk sayı sisteminde bir sayı girin");
+
+        }
+       
+    }
+}
